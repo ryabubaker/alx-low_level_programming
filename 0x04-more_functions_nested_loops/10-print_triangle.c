aaf5933b9f265653971b42bf9dc3e4cebd5ba359
rybabubaker <ryabubaker@gmail.com>
2023-02-25 00:51:16 +0200

    adding task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,16 +8,23 @@
 
 void print_triangle(int size)
 {
-	int i, j, k;
+	int i, j;
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j <= size - i; j++)
-				_putchar(' ');
-			for (k = 1; k <= i; k++)
-				_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				if (j < size - (i + 1))
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
 			_putchar('\n');
 		}
 	}
