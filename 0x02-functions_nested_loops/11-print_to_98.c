8481d3a6da9bed92ec59189d481f0c41c5e42092
rybabubaker <ryabubaker@gmail.com>
2023-02-25 06:30:21 +0200

    adding task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,25 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		printf("%i", i);
-		if (i == 98)
-			break;
-		printf(", ");
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+			if (i == 98)
+				break;
+			printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i", i);
+			if (i == 98)
+				break;
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
