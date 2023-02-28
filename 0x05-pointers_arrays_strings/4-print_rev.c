e697e51ef171404a41b4d4f11d171cd11688652d
rybabubaker <ryabubaker@gmail.com>
2023-03-01 00:20:08 +0200

    adding task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: entry
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (i = i - 1; s[i] != '\0'; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
