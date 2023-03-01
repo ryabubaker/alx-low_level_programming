5f398fc597da43461e78d04490f20948c78cd944
rybabubaker <ryabubaker@gmail.com>
2023-03-01 18:02:08 +0200

    adding task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: entry
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	if (len % 2 == 0)
+	{
+		for (i = (len / 2); str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	if (len % 2 != 0)
+	{
+		for (i = ((len - 1) / 2); str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
