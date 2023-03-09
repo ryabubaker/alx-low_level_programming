e45894c975c2af49bb04ba6738b884ac3a7af45a
rybabubaker <ryabubaker@gmail.com>
2023-03-09 19:48:34 +0200

    adding task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: entry
+ * @needle: entry
+ * Return:  pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int j;
+
+	while (*haystack)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (*(haystack + j) != needle[j])
+				break;
+			if (needle[j + 1] == '\0')
+				return (haystack);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
